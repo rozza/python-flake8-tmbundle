4b67207e329791951f5381dab4cf99a1df010663
Ross Lawley <ross.lawley@gmail.com>
2011-05-17 14:36:06 +0100

    Added doc string tests

--- Support/bin/flake8/pep8.py
+++ Support/bin/flake8/pep8.py
@@ -1225,6 +1225,71 @@ def process_options(arglist=None):
     return options, args
 
 
+
+def docstrings(logical_line, previous_logical):
+    """
+    U of T SPECIFIC:
+    All classes, functions, and methods should contain a docstring.
+    """
+    if previous_logical.startswith("class ") or \
+       previous_logical.startswith("def "):
+        if logical_line[:3].lower() not in ["'''", '"""', "r''", 'r""']:
+            return 0, "UT expected docstring"
+    return
+
+
+def variable_names(logical_line):
+    """
+    U of T SPECIFIC:
+    All classes should be named using CamelCase. All functions and variables
+    should be named using pothole case. Type names should not be used.
+    """
+    # Partial list of types that should not be used in variable names
+    TYPES = ["str", "int", "float", "long", "dict", "list", "file", "type"]
+
+    # Class name checking prohibits underscore (_) in the name.
+    if logical_line.startswith("class "):
+        compound_name = logical_line.split()[1].strip(":")
+        name = compound_name.split("(")[0]
+        if name[0] == "_":
+            name = name[1:]
+        if (not name[0].isupper()) or "_" in name:
+            return 0, "UT expected CamelCase for class name %s" % name
+
+    # Check that function names aren't words that include capitals.
+    elif logical_line.startswith("def "):
+        compound_name = logical_line.split()[1]
+        name = compound_name.split("(")[0]
+        if len(name) > 1 and name.lower() != name:
+            return 0, "UT function name should be pothole case (no capitals)"
+
+        params_plus = logical_line.split("(")[1]
+        params = [p.split("=")[0].strip() \
+                  for p in params_plus.split(")")[0].split(",")]
+        for p in params:
+            if p in TYPES:
+                return 0, "UT parameter name should not be a built-in type"
+            if len(p) > 1 and p.lower() != p:
+                return 0, \
+                "UT parameter name should be pothole case (no capitals)"
+
+    # Assignment statements only check simple assigns, not tuples
+    # Check that name isn't a built-in type. Also check that it either
+    # includes no capitals -- or all capitals (a constant).
+    elif " = " in logical_line:
+        compound_name = logical_line.split("=")[0].strip()
+        name = compound_name.split("[")[0]
+        if name in TYPES:
+            return 0, "UT variable name should not be a built-in type"
+        if len(name) > 1 and \
+           not (name.lower() == name or name.upper() == name):
+            print name
+            return 0, "UT variable name should be pothole case"
+
+    return
+
+
+
 def _main():
     """
     Parse options and run checks on Python source.
